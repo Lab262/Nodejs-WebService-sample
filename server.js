07e563e09dbda7156e6f1f6999488991a07f0fbb
Huallyd <huallyd@gmail.com>
2016-11-08 22:43:21 -0200

    setup project

--- server.js
+++ server.js
@@ -4,50 +4,30 @@
 // =============================================================================================================================
 
 var express    = require('express')
-var cors = require('cors')
-var app        = express()
+var app  = express()
 process.env.NODE_ENV = app.get('env') //set node env
-
 var environment = require('./config/environment')  // get our config file
-var db = require('./config/db')
-var routesSetup = require('./config/routes')
-var jwtHelper = require('./lib/jwthelper')
 
 //Configure app to user bodyParse()
-//this will let use get the data from a post
 var bodyParser = require('body-parser')
-// app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.json({type: 'application/vnd.api+json'}))
-// // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
-// // parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: 'application/vnd.api+json' }))
 
-
+//Allow server side request
+var cors = require('cors')
 app.use(cors());
 
+var db = require('./config/db')
 db.setupDatabase()
 
 app.set('superSecret', environment.secret)  // secret variable
 
-//Block secret urls midlleware
+var jwtHelper = require('./lib/jwthelper')
 app.use(function(req, res, next){
 
-  // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'content-type');
-  // console.log(req.body)
-
-  // Website you wish to allow to connect
-  //res.setHeader('Access-Control-Allow-Origin', '*');
-
-  var isUserPostRoute = ((req.path.indexOf('users') > -1 && req.method === 'POST') || req.path.indexOf('auth') > -1)
+var isUserPostRoute = ((req.path.indexOf('users') > -1 && req.method === 'POST') || req.path.indexOf('auth') > -1)
 
   if (!isUserPostRoute) {
     jwtHelper.verifyJsonWebToken(req,res,next,app)
@@ -55,19 +35,12 @@ app.use(function(req, res, next){
   next()
 })
 
-
-var morgan      = require('morgan')
+//Format debug log 
+var morgan = require('morgan')
 app.use(morgan('dev'))
 
+var routesSetup = require('./config/routes')
 routesSetup.setupRoutesAndVersions(app)
-
-// app.use(function (err, req, res, next) {
-//
-//     // if(err) {
-//     //     return res.send(err.message)
-//     // }
-//     next()
-// })
 
 module.exports = app
 
