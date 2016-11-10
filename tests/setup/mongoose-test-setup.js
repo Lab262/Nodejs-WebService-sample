95e0f27a972858094d590e48aa37c47021b491f7
thiagotmb <tmb0710@gmail.com>
2016-11-10 15:06:11 -0200

    Fixing tests

--- tests/setup/mongoose-test-setup.js
+++ tests/setup/mongoose-test-setup.js
@@ -1,41 +1,39 @@
-'use strict';
+// 'use strict';
 
 
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 
-var environment = require('../../config/environment')
-var mongoose = require('mongoose')
+// var environment = require('../../config/environment')
+// var mongoose = require('mongoose')
 
+// // ensure the NODE_ENV is set to 'test'
+// // this is helpful when you would like to change behavior when testing
 
-// ensure the NODE_ENV is set to 'test'
-// this is helpful when you would like to change behavior when testing
-
-
-beforeEach(function (done) {
+// beforeEach(function (done) {
 
 
-  function clearDB() {
-    for (var i in mongoose.connection.collections) {
-      mongoose.connection.collections[i].remove(function() {});
-    }
-    return done();
-  }
+//   function clearDB() {
+//     for (var i in mongoose.connection.collections) {
+//       mongoose.connection.collections[i].remove(function() {});
+//     }
+//     return done();
+//   }
 
 
-  if (mongoose.connection.readyState === 0) {
-    mongoose.connect(environment.database.host, function (err) {
-      if (err) {
-        throw err;
-      }
-      return clearDB();
-    });
-  } else {
-    return clearDB();
-  }
-});
+//   if (mongoose.connection.readyState === 0) {
+//     mongoose.connect(environment.database.host, function (err) {
+//       if (err) {
+//         throw err;
+//       }
+//       return clearDB();
+//     });
+//   } else {
+//     return clearDB();
+//   }
+// });
 
 
-afterEach(function (done) {
-  mongoose.disconnect();
-  return done();
-});
+// afterEach(function (done) {
+//   mongoose.disconnect();
+//   return done();
+// });
