5678170e8983f81c3afc0ce7ee3a21395a49d95d
thiagotmb <tmb0710@gmail.com>
2016-11-11 02:57:39 -0200

    Adding dynamic url in swagger ui

--- config/swagger-setup.js
+++ config/swagger-setup.js
@@ -1,9 +1,9 @@
 
 
-var environment = require('./environment') 
 
 exports.setupSwagger = function (app, express) {
     
+    var environment = require('./environment') 
     var swaggerJSDoc = require('swagger-jsdoc');
     // swagger definition
     var swaggerDefinition = {
@@ -13,18 +13,20 @@ exports.setupSwagger = function (app, express) {
             description: 'OhMyBox '
         },
         host: environment.server.host,
-        basePath: '/'
-    };
+        basePath: '/'    };
     // options for the swagger docs
     var options = {
         // import swaggerDefinitions
         swaggerDefinition: swaggerDefinition,
         // path to the API docs
-        apis: ['./server/v0/routes/*.js']
+        apis: ['./server/v0/routes/*.js'],
+                url:  "dasdsa"
+
     };
     // initialize swagger-jsdoc
     var swaggerSpec = swaggerJSDoc(options);
-
+    
+    
     app.get('/swagger.json', function (req, res) {
         // res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
@@ -34,5 +36,18 @@ exports.setupSwagger = function (app, express) {
     app.set('views', path.join('./', 'views'));
     app.set('view engine', 'jade');
     app.use(express.static(path.join( './', 'public')));
+
+
+    let newHostUrl = "            url = " + `\"${environment.server.protocol}${environment.server.host}/swagger.json\"`;
+    //replace the host of swagger ui
+    var fs = require('fs');
+    var data = fs.readFileSync('public/api-docs/index.html', 'utf-8');
+    var lines = data.split('\n');
+    // remove one line, starting at the first position
+    lines.splice(43,1,newHostUrl);
+    // join the array back into a single string
+    var newtext = lines.join('\n');
+    var newValue = newtext;
+    fs.writeFileSync('public/api-docs/index.html', newValue, 'utf-8');
 
 }
