58a8fdc39182c53955cc28f1dc510834c00d738d
Huallyd <huallyd@gmail.com>
2016-11-15 03:28:54 -0200

    Get all users with page, limit and querys

--- server/v0/routes/users.js
+++ server/v0/routes/users.js
@@ -8,48 +8,124 @@ var objectSerializer = require('../../../lib/object-serializer')
 var models = require('../models/index');
 
 
+/**
+ * @swagger
+ * /api/v0/users:
+ *   get:
+ *     tags:
+ *       - Users
+ *     description: get all users
+ *     parameters:
+*       - name: page
+*         description: number of pages
+*         in: query
+*         required: false
+*         type: integer
+*       - name: limit
+*         description: skip number
+*         in: query
+*         required: false
+*         type: integer
+*       - name: where
+*         description: regex for sequelize example _ email iLike ? OR email iLike ? \n %smad% \n %th% , each line represents a item in array. iLike %th%  means that will search all emails that contains the without consider the sensitive case. For each ? must have a parameter to corresponds like the %th% 
+*         in: query
+*         required: false
+*         type: array
+*         collectionFormat: multi
+*       - name: x-access-token
+*         description: access token user
+*         in: header
+*         required: true
+*         type: string
+ *     responses:
+ *       200:
+ *         description: all users.
+ *       404:
+ *         description: User not authorized.
+ */
 router.route('/users')
 
   .get(function (req, res) {
 
-    // var token = req.headers['x-access-token']
-    // var decodedUser = Jwt.decode(token);
+/* query param format
+[email iLike ? OR email iLike ?,
+%smad%, 
+%th%]
+*/
 
-    // var pageVariables = objectSerializer.deserializeQueryPaginationIntoVariables(req)
-    // var totalLength = 0
+    var token = req.headers['x-access-token']
+    var decodedUser = Jwt.decode(token);
 
-    // User.findOne({ _id: decodedUser.id}).exec().then(function(user) {
-    //   if (user.isAdmin) {
+    var pageVariables = objectSerializer.deserializeQueryPaginationIntoVariables(req)
+    var totalLength = 0
 
-    //     return User.count(req.query).exec()
-    //   } else {
+    models.User.findOne({ where: { id: decodedUser.id } }).then(function (user) {
 
-    //     var serialized = objectSerializer.serializeObjectIntoJSONAPI(users)
-    //     return res.json(serialized)
-    //   }
+      if (user === null) {
+        var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify("users not found"))
+          return res.status(404).json(error)
+      }
+      if (user.accessLevel == 2) {
 
-    // }).then(function(count) {
+        var queryDict = {
+          where: req.query.where  
+        }
+        if (pageVariables.limit != 0 || pageVariables.skip != 0) {
+          queryDict = {
+            offset: pageVariables.skip,
+            limit: pageVariables.limit,
+            where: req.query.where
+          }
+        }
 
-    //   totalLength = count
+        return models.User.findAndCountAll(queryDict).then(function (result) {
+          if (result.length < 1) {
+            return res.status(200).json({ data: [] });
+          } else {
+            var serialized = objectSerializer.serializeObjectIntoJSONAPI(result.rows, result.count, pageVariables.limit)
+            return res.json(serialized)
+          }
+        }).catch(function (err) {
+          var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify(err))
+          return res.status(403).json(error)
+        })
 
-    //   if (totalLength > 0) {
-
-    //     return User.find(req.query).skip(pageVariables.skip).limit(pageVariables.limit).sort({ isAdmin: 'descending'}).exec()
-    //   } else {
-
-    //     return res.status(200).json({data: []});
-    //   }
-    // }).then(function(users) {
-
-    //   var serialized = objectSerializer.serializeObjectIntoJSONAPI(users, totalLength, pageVariables.limit)
-    //   return res.json(serialized)
-    // }).then(function(err) {
-
-    //   var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify(err))
-    //   return res.status(403).json(error)
-    // })
-
+      } else {
+        return res.status(200).json(user)
+      }
+    })
   })
+
+  //User.findOne({ _id: decodedUser.id}).exec().then(function(user) {
+  //   if (user.isAdmin) {
+
+  //     return User.count(req.query).exec()
+  //   } else {
+
+  //     var serialized = objectSerializer.serializeObjectIntoJSONAPI(users)
+  //     return res.json(serialized)
+  //   }
+
+  // }).then(function(count) {
+
+  //   totalLength = count
+
+  //   if (totalLength > 0) {
+
+  //     return User.find(req.query).skip(pageVariables.skip).limit(pageVariables.limit).sort({ isAdmin: 'descending'}).exec()
+  //   } else {
+
+  //     return res.status(200).json({data: []});
+  //   }
+  // }).then(function(users) {
+
+  //   var serialized = objectSerializer.serializeObjectIntoJSONAPI(users, totalLength, pageVariables.limit)
+  //   return res.json(serialized)
+  // }).then(function(err) {
+
+  //   var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify(err))
+  //   return res.status(403).json(error)
+  // })
 
 
   /**
@@ -60,17 +136,22 @@ router.route('/users')
    *       - Users
    *     description: Create new user
    *     parameters:
+  *       - name: isEmailVerified
+  *         description: is email verified
+  *         in: formData
+  *         required: false
+  *         type: boolean
   *       - name: email
   *         description: user valid email
   *         in: formData
   *         required: true
   *         type: string
   *       - name: password
-  *         description: user  password
+  *         description: user password
   *         in: formData
   *         required: true
   *         type: string
-  *       - name: access level
+  *       - name: accessLevel
   *         description: access level type options are 0 = user, 1 = seller, 2 = admin.
   *         in: formData
   *         required: true
@@ -129,45 +210,50 @@ router.route('/users')
 
   })
 
- /**
-   * @swagger
-   * /api/v0/users/{id}:
-   *   patch:
-   *     tags:
-   *       - Users
-   *     description: get user and update by id
-   *     parameters:
-  *       - name: id
-  *         description: user valid id
-  *         in: path
-  *         required: true
-  *         type: string
-  *       - name: x-access-token
-  *         description: access token user
-  *         in: header
-  *         required: true
-  *         type: string
-  *       - name: email
-  *         description: user email
-  *         in: formData
-  *         required: false
-  *         type: string
-  *       - name: name
-  *         description: user name
-  *         in: formData
-  *         required: false
-  *         type: string
-  *       - name: gender
-  *         description: user gender
-  *         in: formData
-  *         required: false
-  *         type: string
-   *     responses:
-   *       200:
-   *         description: User successfully updated.
-   *       404:
-   *         description: User not found.
-   */
+/**
+  * @swagger
+  * /api/v0/users/{id}:
+  *   patch:
+  *     tags:
+  *       - Users
+  *     description: get user and update by id
+  *     parameters:
+ *       - name: id
+ *         description: user valid id
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: x-access-token
+ *         description: access token user
+ *         in: header
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: user email
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: name
+ *         description: user name
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: gender
+ *         description: user gender
+ *         in: formData
+ *         required: false
+ *         type: string
+ *       - name: accessLevel
+ *         description: access level type options are 0 = user, 1 = seller, 2 = admin.
+ *         in: formData
+ *         required: false
+ *         type: int
+  *     responses:
+  *       200:
+  *         description: User successfully updated.
+  *       404:
+  *         description: User not found.
+  */
 router.route('/users/:id')
 
   .patch(function (req, res) {
@@ -258,16 +344,17 @@ router.route('/users/:id')
   .delete(function (req, res) {
 
     models.User.destroy({
-    where: {
+      where: {
         id: req.params.id
-    }}).then(function (result) {
+      }
+    }).then(function (result) {
 
       if (result == 0) {
         var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify("User not found."))
         return res.status(404).json(error)
       } else {
         return res.status(200).json("User successfully deleted")
-      } 
+      }
     }).catch(function (err) {
       var error = objectSerializer.serializeSimpleErrorIntoJSONAPI(JSON.stringify(err))
       return res.status(404).json(error)
